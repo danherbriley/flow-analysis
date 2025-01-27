dd6e679e3a8c2836b3da8047ce91864938500ee4
Daniel Riley <dan.riley@seznam.cz>
2025-01-27 03:14:25 +0200

    implement create study script

---
+++ scripts/create_study.py
@@ -0,0 +1,6 @@
+import optuna
+# workaround to be able to create study with more refined params
+
+study_name = "/project/project_465001585/viry/flow-analysis/data"
+storage_name = f"sqlite:///{study_name}.db"
+optuna.create_study(direction="minimize", pruner=optuna.pruners.MedianPruner(n_warmup_steps=10), storage=storage_name, study_name=study_name)
